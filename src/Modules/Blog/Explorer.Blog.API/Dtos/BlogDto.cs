2e149dd2544dc05e47aad04d67520677b6d99935
Natalija Popović <popovic.ra117.2020@uns.ac.rs>
2023-10-15 18:46:27 +0200

    Created domain and service classes for Blog Module

---
+++ src/Modules/Blog/Explorer.Blog.API/Dtos/BlogDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Blog.API.Dtos
+{
+    public enum BlogStatus { DRAFT = 1, PUBLISHED, CLOSED };
+    public class BlogDto
+    {
+        public String Title { get; set; }
+        public String Description { get; set; }
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        public BlogStatus Status { get; set; } = BlogStatus.DRAFT;
+        public List<string>? Images { get; set; }
+    }
+}
---
+++ src/Modules/Blog/Explorer.Blog.Core/Domain/Blog.cs
@@ -0,0 +1,31 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Blog.Core.Domain
+{
+    public enum BlogStatus { DRAFT = 1, PUBLISHED, CLOSED };
+    public class Blog: Entity
+    {
+        public String Title { get; init; }
+        public String Description { get; init; }
+        public DateTime CreationDate { get; init; } = DateTime.Now;
+        public BlogStatus Status { get; init; } = BlogStatus.DRAFT;
+        public List<string>? Images { get; init; }
+
+        public Blog(string title, string description, DateTime creationDate,
+            BlogStatus status, List<string>? images)
+        {
+            if(string.IsNullOrEmpty(title)) throw new ArgumentException("Invalid or empty title.");
+            if(string.IsNullOrEmpty(description)) throw new ArgumentException("Invalid or empty description.");
+            Title = title; 
+            Description = description;
+            CreationDate = creationDate;
+            Status = status;
+            Images = images;
+        }
+    }
+}
