a295852212301d483f96490e2890d00b2ee946a7
Natalija Popović <popovic.ra117.2020@uns.ac.rs>
2023-10-15 21:57:02 +0200

    Create controller for Blog in Explorer.API

---
+++ src/Explorer.API/Controllers/Stakeholder/Blogging/BlogController.cs
@@ -0,0 +1,26 @@
+﻿using Explorer.Blog.API.Dtos;
+using Explorer.Blog.API.Public;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Stakeholder.Blogging
+{
+    [Authorize(Policy ="touristPolicy")]
+    [Authorize(Policy ="authorPolicy")]
+    [Route("api/blogging/blog")]
+    public class BlogController: BaseApiController
+    {
+        private readonly IBlogService _blogService;
+        public BlogController(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+        [HttpPost]
+        public ActionResult<BlogDto> Create([FromBody] BlogDto blog)
+        {
+            var result = _blogService.Create(blog);
+            return CreateResponse(result);
+        }
+    }
+}
