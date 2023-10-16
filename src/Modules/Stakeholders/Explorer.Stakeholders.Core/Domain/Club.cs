307721878d08668394d3df2cfcfc430fe0605011
Luka Milanko <milankoluka01@gmail.com>
2023-10-16 23:33:22 +0200

    kreiranje zavrseno

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/Club.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Explorer.BuildingBlocks.Core.Domain;
+
+namespace Explorer.Stakeholders.Core.Domain
+{
+    public class Club : Entity
+    {
+        public string Name { get; init; }
+        public string Description { get; init; }
+        public string URL { get; init; }
+
+        public Club() { }
+        public Club(string name, string description, string url)
+        {
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Invalid Name.");
+            Name = name;
+            Description = description;
+            URL = url;
+        }
+    }
+}
