1bd4340287b0dd8905832e134e1a1b2ce6473973
Jovana Ristic <jovana012002@gmail.com>
2023-10-17 23:10:47 +0200

    feat: tour problem features added

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/ProblemDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.API.Dtos
+{
+    public class ProblemDto
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public bool Priority { get; set; }
+        public string Description { get; set; }
+        public DateTime Time { get; set; }
+        public int TourId { get; set; }
+    }
+}
