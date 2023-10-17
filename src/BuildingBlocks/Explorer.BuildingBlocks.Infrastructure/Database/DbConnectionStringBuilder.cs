68f3b6bef5fa45cf08255b4860cd2052e904c226
Jovan Dozic <jovandozic@gmail.com>
2023-10-17 08:57:50 +0200

    Testing

--- src/BuildingBlocks/Explorer.BuildingBlocks.Infrastructure/Database/DbConnectionStringBuilder.cs
+++ src/BuildingBlocks/Explorer.BuildingBlocks.Infrastructure/Database/DbConnectionStringBuilder.cs
@@ -9,7 +9,7 @@ public static class DbConnectionStringBuilder
         var database = Environment.GetEnvironmentVariable("DATABASE_SCHEMA") ?? "explorer-v1";
         var schema = Environment.GetEnvironmentVariable("DATABASE_SCHEMA_NAME") ?? schemaName;
         var user = Environment.GetEnvironmentVariable("DATABASE_USERNAME") ?? "postgres";
-        var password = Environment.GetEnvironmentVariable("DATABASE_PASSWORD") ?? "super";
+        var password = Environment.GetEnvironmentVariable("DATABASE_PASSWORD") ?? "postgres";
         var integratedSecurity = Environment.GetEnvironmentVariable("DATABASE_INTEGRATED_SECURITY") ?? "false";
         var pooling = Environment.GetEnvironmentVariable("DATABASE_POOLING") ?? "true";
 
@@ -17,3 +17,5 @@ public static class DbConnectionStringBuilder
             $"Server={server};Port={port};Database={database};SearchPath={schema};User ID={user};Password={password};Integrated Security={integratedSecurity};Pooling={pooling};";
     }
 }
+
+// Testing git ignore.
