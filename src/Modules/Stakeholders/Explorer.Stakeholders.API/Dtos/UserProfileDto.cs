7e65652cdcf3fce6809fb0a05a5043f3eec9c474
Marko Košarac <marko_kosarac@hotmail.com>
2023-10-21 12:58:21 +0200

    feat: test commit

--- src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/UserProfileDto.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/UserProfileDto.cs
@@ -2,7 +2,7 @@
 
 public class UserProfileDto
 {
-    public int ss;
+    public int sasa;
     public string Name { get; set; }
     public string Surname { get; set; }
     public string Picture { get; set; }
