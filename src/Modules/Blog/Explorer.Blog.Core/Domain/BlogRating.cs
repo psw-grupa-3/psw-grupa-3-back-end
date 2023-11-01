226ca61fe1a134ce2830d7311a97dd1c7c537c22
Natalija Popović <popovic.ra117.2020@uns.ac.rs>
2023-11-01 18:43:59 +0100

    Created BlogRating value object and aggregate Blog

---
+++ src/Modules/Blog/Explorer.Blog.Core/Domain/BlogRating.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Explorer.BuildingBlocks.Core.Domain;
+
+namespace Explorer.Blog.Core.Domain
+{
+    public enum Vote {PLUS = 1, MINUS}
+    public class BlogRating: ValueObject
+    {
+
+        public long BlogId { get; init; }
+        public long UserId { get; init; }
+        public DateTime VotingDate { get; init; }
+        public Vote Mark { get; init; }
+
+
+        [JsonConstructor]
+        public BlogRating(long blogId, long userId, DateTime votingDate, Vote mark)
+        {
+            BlogId = blogId;
+            UserId = userId;
+            VotingDate = votingDate;
+            Mark = mark;
+        }
+
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return BlogId;
+            yield return UserId;
+            yield return VotingDate;
+            yield return Mark;
+        }
+    }
+}
--- src/Modules/Blog/Explorer.Blog.Infrastructure/Database/BlogContext.cs
+++ src/Modules/Blog/Explorer.Blog.Infrastructure/Database/BlogContext.cs
@@ -6,10 +6,12 @@ namespace Explorer.Blog.Infrastructure.Database;
 public class BlogContext : DbContext
 {
     public DbSet<Core.Domain.Blog> Blogs {  get; set; }
-    public DbSet<BlogComment> BlogComments { get; set; }
     public BlogContext(DbContextOptions<BlogContext> options) : base(options) {}
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema("blog");
+
+        modelBuilder.Entity<Core.Domain.Blog>()
+            .Property(item => item.Ratings).HasColumnType("jsonb");
     }
 }
