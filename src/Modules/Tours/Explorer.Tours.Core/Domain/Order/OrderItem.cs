2d31acf2524857f092eefa4a4c51dc5388a9e6ce
Luka Milanko <milankoluka01@gmail.com>
2023-11-02 11:00:29 +0100

    classes shopping cart and orderItem

---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/Order/OrderItem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Explorer.BuildingBlocks.Core.Domain;
+using Microsoft.Extensions.Options;
+
+namespace Explorer.Tours.Core.Domain.Order
+{
+    public class OrderItem : ValueObject
+    {
+        public int IdTour { get; init; }
+        public string Name { get; init; }
+        public double Price { get; init; }
+        public OrderItem(int idTour, string name, double price)
+        {
+            IdTour = idTour;
+            Name = name;
+            Price = price;
+            Validate();
+        }
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return IdTour;
+            yield return Name;
+            yield return Price;
+        }
+        private void Validate()
+        {
+            if (string.IsNullOrEmpty(Name)) throw new ArgumentException("Invalid name");
+            if (Price < 0) throw new ArgumentException("Invalid price");
+            if (IdTour == 0) throw new ArgumentException("Invalid TourId");
+        }
+
+    }
+}
---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/Order/ShoppingCart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Explorer.BuildingBlocks.Core.Domain;
+using Microsoft.Extensions.Options;
+
+namespace Explorer.Tours.Core.Domain.Order
+{
+    public class ShoppingCart : Entity
+    {
+        public int IdUser { get; init; }
+        public List<OrderItem> Items { get; init; }
+        public ShoppingCart(int idUser, List<OrderItem> items)
+        {
+            IdUser = idUser;
+            Items = items;
+            Validate();
+        }
+        private void Validate()
+        {
+            if (IdUser == 0) throw new ArgumentException("Invalid UserId");
+        }
+
+    }
+}
