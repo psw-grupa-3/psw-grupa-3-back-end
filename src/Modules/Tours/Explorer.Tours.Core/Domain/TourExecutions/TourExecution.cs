5d4fe4d3c8f00a4e6bb77125f398709f6b57a536
Natalija Popović <popovic.ra117.2020@uns.ac.rs>
2023-11-05 22:24:56 +0100

    Minor refactor at updatePosition method of TourExecution

--- src/Modules/Tours/Explorer.Tours.Core/Domain/TourExecutions/TourExecution.cs
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/TourExecutions/TourExecution.cs
@@ -6,10 +6,11 @@ namespace Explorer.Tours.Core.Domain.TourExecutions
     public enum TourExecutionStatus {ACTIVE = 1, COMPLETED, ABANDONED}
     public class TourExecution: Entity
     {
+        private const int PointProximity = 100;
         public long UserId { get; init; }
         public TourExecutionStatus Status { get; set; }
         public Position Position { get; set; }
-        public List<Task> Tasks { get; init; }
+        public List<Task> Tasks { get; private set; }
         
         public TourExecution(long id, long userId, List<Task> tasks, Position position)
         {
@@ -22,16 +23,20 @@ namespace Explorer.Tours.Core.Domain.TourExecutions
 
         public void updatePosition(Position currentPosition)
         {
-            Position = currentPosition;
-
-            List<PointTask> pointTasks = Tasks.FindAll(x => x.Type == TaskType.POINT)
-                .OfType<PointTask>().ToList();
-            PointTask currentPoint = pointTasks.FirstOrDefault(x => !x.Done) ?? throw new Exception("Exception! All Tour Points passed!");
-
-            currentPoint.Done = DistanceCalculator.CalculateDistance(currentPoint, currentPosition) * 1000 <= 100;
-            Status = pointTasks.TrueForAll(x => x.Done) ? TourExecutionStatus.COMPLETED : Status;
+            if (Status == TourExecutionStatus.ACTIVE)
+            {
+                Position = currentPosition;
+                var pointTasks = Tasks.FindAll(x => x.Type == TaskType.POINT).OfType<PointTask>().ToList();
+                var currentPoint = pointTasks.FirstOrDefault(x => !x.Done) ?? throw new Exception("Exception! All Tour Points passed!");
+                var inProximity = DistanceCalculator.CalculateDistance(currentPoint, currentPosition) * 1000 <= PointProximity;
+                if (inProximity)
+                {
+                    currentPoint.Done = true;
+                    currentPoint.DoneOn = currentPosition.LastActivity;
+                }
+                Status = pointTasks.TrueForAll(x => x.Done) ? TourExecutionStatus.COMPLETED : Status;
+            }
         }
-
         public void quitTourExecution()
         {
             Status = TourExecutionStatus.ABANDONED;
