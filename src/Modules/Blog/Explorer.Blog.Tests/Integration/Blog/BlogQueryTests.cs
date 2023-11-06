57a3ee4bb1d0a505280f1e462f0520cc0f3518e6
Natalija Popović <popovic.ra117.2020@uns.ac.rs>
2023-11-06 13:46:34 +0100

    Added CommandTests for Blog

---
+++ src/Modules/Blog/Explorer.Blog.Tests/Integration/Blog/BlogQueryTests.cs
@@ -0,0 +1,30 @@
+﻿using Explorer.API.Controllers.Community;
+using Explorer.Blog.API.Public;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Explorer.Blog.Tests.Integration.Blog
+{
+    [Collection("Sequential")]
+    public class BlogQueryTests : BaseBlogIntegrationTest
+    {
+        public BlogQueryTests(BlogTestFactory factory) : base(factory) { }
+
+        [Fact]
+        public void Retrieves_all()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        private static BlogController CreateController(IServiceScope scope)
+        {
+            return new BlogController(scope.ServiceProvider.GetRequiredService<IBlogService>())
+            {
+                ControllerContext = BuildContext("-1")
+            };
+        }
+    }
+}
