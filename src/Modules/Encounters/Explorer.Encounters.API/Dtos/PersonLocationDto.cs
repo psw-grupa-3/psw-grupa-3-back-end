eaf4c2efa5f8eacada0b95a0dc348088bd2aae95
Natalija Popović <popovic.ra117.2020@uns.ac.rs>
2023-12-03 14:20:38 +0100

    test: Encounter activation test initiate

---
+++ src/Modules/Encounters/Explorer.Encounters.API/Dtos/PersonLocationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Explorer.Encounters.API.Dtos
+{
+    public class PersonLocationDto
+    {
+        public long PersonId { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+    }
+}
--- src/Modules/Encounters/Explorer.Encounters.Tests/Integration/Encounter/EncounterCommandTests.cs
+++ src/Modules/Encounters/Explorer.Encounters.Tests/Integration/Encounter/EncounterCommandTests.cs
@@ -1,10 +1,47 @@
-﻿namespace Explorer.Encounters.Tests.Integration.Encounter
+﻿using Explorer.API.Controllers.Encounter;
+using Explorer.Encounters.API.Dtos;
+using Explorer.Encounters.API.Public;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.Encounters.Tests.Integration.Encounter
 {
     [Collection("Sequential")]
     public class EncounterCommandTests: BaseEncounterIntegrationTest
     {
-        public EncounterCommandTests(EncounterTestFactory factory) : base(factory)
+        public EncounterCommandTests(EncounterTestFactory factory) : base(factory){}
+        
+           [Fact]
+           public void Activates()
+            {
+                // Arrange
+                using var scope = Factory.Services.CreateScope();
+                var controller = CreateController(scope);
+                var enocunterId = -1;
+                var personLocation = new PersonLocationDto
+                {
+                    PersonId = -1,
+                    Latitude = 0.0,
+                    Longitude = 0.0,
+                };
+
+                // Act
+                var result = ((ObjectResult)controller.Activate(enocunterId, personLocation).Result)?.Value as EncounterDto;
+
+                // Assert - Response
+                result.ShouldNotBeNull();
+
+                // Assert - Database
+            }
+        
+
+        private static EncounterController CreateController(IServiceScope scope)
         {
+            return new EncounterController(scope.ServiceProvider.GetRequiredService<IEncounterService>())
+            {
+                ControllerContext = BuildContext("-1")
+            };
         }
     }
 }
