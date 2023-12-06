ac345a9148b9e3b52bf849e6b675cf5dd4a50d72
Natalija Popović <popovic.ra117.2020@uns.ac.rs>
2023-12-06 19:00:52 +0100

    fix: Changed test data and also added a condition stating that no active encounter shouldn't be activated

--- src/Modules/Encounters/Explorer.Encounters.Tests/TestData/b-encounter.sql
+++ src/Modules/Encounters/Explorer.Encounters.Tests/TestData/b-encounter.sql
@@ -1,12 +1,12 @@
 INSERT INTO encounters."Encounters"(
 	"Id", "Name", "Description", "Location", "Experience", "Status", "Type",  "Radius", "Participants", "Completers")
-	VALUES (-1, 'Encounter -1', 'Encounter -1', '{{"Latitude": 45.244442, "Longitude": 19.841521}}', 10, 1, 1, 100,'[]', '[]');
+	VALUES (-1, 'Encounter -1', 'Encounter -1', '{{"Latitude": 45.244442, "Longitude": 19.841521}}', 10, 2, 1, 100,'[]', '[]');
 INSERT INTO encounters."Encounters"(
 	"Id", "Name", "Description", "Location", "Experience", "Status", "Type", "Radius","Participants", "Completers")
-	VALUES (-2, 'Encounter -2', 'Encounter -2', '{{"Latitude": 45.244442, "Longitude": 19.841521}}', 15, 1, 1, 100, '[{{"Username": "turista1@gmail.com"}}]', '[]');
+	VALUES (-2, 'Encounter -2', 'Encounter -2', '{{"Latitude": 45.244442, "Longitude": 19.841521}}', 15, 2, 1, 100, '[{{"Username": "turista1@gmail.com"}}]', '[]');
 INSERT INTO encounters."Encounters"(
 	"Id", "Name", "Description", "Location", "Experience", "Status", "Type", "Radius","Participants", "Completers")
-	VALUES (-3, 'Encounter -3', 'Encounter -3', '{{"Latitude": 37.971413, "Longitude": 23.726136}}', 20, 1, 1, 15, '[{{"Username": "turista1@gmail.com"}}, {{"Username": "turista2@gmail.com"}}, {{"Username": "turista3@gmail.com"}}]', '[]');
+	VALUES (-3, 'Encounter -3', 'Encounter -3', '{{"Latitude": 37.971413, "Longitude": 23.726136}}', 20, 2, 1, 15, '[{{"Username": "turista1@gmail.com"}}, {{"Username": "turista2@gmail.com"}}, {{"Username": "turista3@gmail.com"}}]', '[]');
 INSERT INTO encounters."Encounters"(
 	"Id", "Name", "Description", "Location", "Experience", "Status", "Type", "Radius","Participants", "Completers")
-	VALUES (-4, 'Encounter -4', 'Encounter -4', '{{"Latitude": 44.654072, "Longitude": 21.147595}}', 20, 1, 1, 50, '[{{"Username": "turista1@gmail.com"}}, {{"Username": "turista2@gmail.com"}}, {{"Username": "turista3@gmail.com"}}]', '[]');
+	VALUES (-4, 'Encounter -4', 'Encounter -4', '{{"Latitude": 44.654072, "Longitude": 21.147595}}', 20, 2, 1, 50, '[{{"Username": "turista1@gmail.com"}}, {{"Username": "turista2@gmail.com"}}, {{"Username": "turista3@gmail.com"}}]', '[]');
