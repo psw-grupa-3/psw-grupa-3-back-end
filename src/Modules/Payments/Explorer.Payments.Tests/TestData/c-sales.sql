3dc94665f776901fa3c9ec47cd60debc8d3af69a
Natalija Popović <popovic.ra117.2020@uns.ac.rs>
2023-12-06 22:57:52 +0100

    fix: test data changed again

--- src/Modules/Payments/Explorer.Payments.Tests/TestData/c-sales.sql
+++ src/Modules/Payments/Explorer.Payments.Tests/TestData/c-sales.sql
@@ -1,6 +1,6 @@
 INSERT INTO payments."Sales"(
-    "Id", "ToursOnSale", "SaleStart", "SaleEnd", "DiscountPercentage", "IsActive")
-VALUES (-1,0,'2023-01-01', '2023-01-31', 10, false)
+    "Id", "JsonObject")
+VALUES (-1,'{{"ToursOnSale": 0, "SaleStart": "2023-01-01", "SaleEnd" : "2023-01-31", "DiscountPercentage" : 10, "IsActive": false}}');
 INSERT INTO payments."Sales"(
     "Id", "ToursOnSale", "SaleStart", "SaleEnd", "DiscountPercentage", "IsActive")
-VALUES (-2,0,'2023-02-01', '2023-02-31', 10, true)
+VALUES (-2,'{{"ToursOnSale": 0, "SaleStart": "2023-01-01", "SaleEnd" : "2023-01-31", "DiscountPercentage" : 10, "IsActive": true}}');
