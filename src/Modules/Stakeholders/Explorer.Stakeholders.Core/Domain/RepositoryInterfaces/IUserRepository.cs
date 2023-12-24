9b481713e83b754ecd37fb04a7dc12bfc59ba616
Natalija Popović <popovic.ra117.2020@uns.ac.rs>
2023-12-24 14:07:31 +0100

    feat: Created method for checking if user with provided email exists also created PasswordResetToken

--- src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/RepositoryInterfaces/IUserRepository.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/RepositoryInterfaces/IUserRepository.cs
@@ -5,6 +5,7 @@ namespace Explorer.Stakeholders.Core.Domain.RepositoryInterfaces
     public interface IUserRepository
     {
         bool Exists(string username);
+        bool ExistsByEmail(string email);
         User? GetActiveByName(string username);
         User Create(User user);
         long GetPersonId(long userId);
--- src/Modules/Stakeholders/Explorer.Stakeholders.Infrastructure/Database/Repositories/UserDatabaseRepository.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Infrastructure/Database/Repositories/UserDatabaseRepository.cs
@@ -19,6 +19,11 @@ namespace Explorer.Stakeholders.Infrastructure.Database.Repositories
             return _dbContext.Users.Any(user => user.Username == username);
         }
 
+        public bool ExistsByEmail(string email)
+        {
+            return _dbContext.Users.Any(user => user.Email == email);
+        }
+
         public User? GetActiveByName(string username)
         {
             return _dbContext.Users.FirstOrDefault(user => user.Username == username && user.IsActive && user.IsProfileActivated);
